a0d8a1297104331176a03cf7fea9526962dc2bb4
- <->
2024-02-10 20:13:59 +0300

    uploaded v1

--- leetcode/polindromic-substrings.py
+++ leetcode/polindromic-substrings.py
@@ -1,32 +1,33 @@
 class Solution:
-    def countSubstrings(self, s: str) -> int:
-        # get all the sub strings
-        sub_strings = []
-        for l in range(len(s)):
-            pass
+    def is_polindrom(self, s: str) -> True:
+        end = len(s) - 1
+        if end == 0:
+            return True
         
-def is_polindrom(s: str) -> True:
-    end = len(s) - 1
-    if end == 0:
+        i = 0
+        while i < (len(s) / 2):
+            if s[0 + i] != s[end - i]:
+                return False
+            i += 1
         return True
     
-    i = 0
-    while i < (len(s) / 2):
-        if s[0 + i] != s[end - i]:
-            return False
-        i += 1
-    return True
+    def get_substrings(self, s:str) -> list:
+        substrings = []
+        #   getting all substrings
+        for i in range(1, len(s) + 1):
+                for j in range(len(s) - i + 1):
+                    sub = s[j: j+i]
+                    # print(f"i:{i}, j:{j} -> {sub}")
+                    substrings.append(sub)
         
-if __name__ == "__main__":
-    s = "aabcad"
-    substrings = []
-    #   getting all substrings
-    for i in range(1, len(s) + 1):
-            for j in range(len(s) - i + 1):
-                sub = s[j: j+i]
-                print(f"i:{i}, j:{j} -> {sub}")
-                substrings.append(sub)
-    print("----------------------------")        
-    for ss in substrings:
-        if is_polindrom(ss):
-            print(ss)
+        return substrings
+
+    def countSubstrings(self, s: str) -> int:
+        # get all the sub strings
+        subs = self.get_substrings(s)
+        count = 0
+        for sub in subs:
+            if self.is_polindrom(sub):
+                count += 1
+
+        return count
