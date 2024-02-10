83a9d1d68fd66ce25137301093881829baac6447
- <->
2024-02-10 20:13:19 +0300

    working solution 1

--- leetcode/polindromic-substrings.py
+++ leetcode/polindromic-substrings.py
@@ -1,6 +1,3 @@
-
-
-
 class Solution:
     def countSubstrings(self, s: str) -> int:
         # get all the sub strings
@@ -8,10 +5,28 @@ class Solution:
         for l in range(len(s)):
             pass
         
+def is_polindrom(s: str) -> True:
+    end = len(s) - 1
+    if end == 0:
+        return True
+    
+    i = 0
+    while i < (len(s) / 2):
+        if s[0 + i] != s[end - i]:
+            return False
+        i += 1
+    return True
         
 if __name__ == "__main__":
     s = "aabcad"
+    substrings = []
     #   getting all substrings
     for i in range(1, len(s) + 1):
             for j in range(len(s) - i + 1):
-                print(f"i:{i}, j:{j} -> {s[j: j+i]}")
+                sub = s[j: j+i]
+                print(f"i:{i}, j:{j} -> {sub}")
+                substrings.append(sub)
+    print("----------------------------")        
+    for ss in substrings:
+        if is_polindrom(ss):
+            print(ss)
