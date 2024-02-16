ed5e296dd3a7858031bccd9a82a8d1c3c40d6032
- <->
2024-02-17 00:35:54 +0300

    min-window: run time improvement, memory fail

--- leetcode/min-window.py
+++ leetcode/min-window.py
@@ -1,36 +1,40 @@
 #   https://leetcode.com/problems/minimum-window-substring/?envType=daily-question&envId=2024-02-12
 
 import copy
+from collections import Counter
 
 class Solution:
-    #   TODO: could be more efficient
-    def chars_are_in(self, sub:str, t:str):
-        sub_copy = copy.copy(sub)
-        for c in t:
-            if c not in sub_copy:
-                return False
-            sub_copy = sub_copy.replace(c,"",1)
-        return True
+    def chars_are_in(self, s:Counter, t:Counter) -> bool:
+        res = t - s
+        #   cause 0 means all of t is in s and we want it to be true
+        return not res
+        
     
     def minWindow(self, s: str, t: str) -> str:
         right = 0
         left = 0
         m = len(s)
         n = len(t)
+        s_counter = Counter("")
+        t_counter = Counter(t)
         if n > m:
             return ""
         
+        #   technically i could stop whenever i couldn't move the right or left pointers any more, and that would be the actual solution
+        #   instead of returning the min
         subs_that_contain = []
         
         #   while both pointers and in side the main string
         #   TODO: check about the validity of while right > left and terminate when left passes right
         while right < m and left < m:
             #   move right till we get everything inside
-            while (not self.chars_are_in(t=t, sub= s[left:right])) and right < m:
+            while (not self.chars_are_in(t=t_counter, s = s_counter)) and right < m:
+                s_counter[s[right]] = s_counter[s[right]] + 1
                 right += 1
             
             #   move left while we still contain everything to shorten
-            while self.chars_are_in(t=t, sub= s[left:right]) and left < m:
+            while self.chars_are_in(t=t_counter, s = s_counter) and left < m:
+                s_counter[s[left]] = s_counter[s[left]] - 1
                 subs_that_contain.append(s[left:right])
                 left += 1
                 
