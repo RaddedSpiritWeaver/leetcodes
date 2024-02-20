b96329dd17fc804f6c26fc525f1766740d51cd22
- <->
2024-02-20 11:48:35 +0300

    missing-number: init, accepted

---
+++ leetcode/missing-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        index = 0
+        for i in range(len(nums) + 1):
+            if index == len(nums):
+                return index
+            if index != nums[i]:
+                return index
+            index += 1
+            
+if __name__ == "__main__":
+    s = Solution()
+    nums = [9,6,4,2,3,5,7,0,1]
+    print(Solution.missingNumber(nums))
