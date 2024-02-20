6140dbd304d8595284dbe772fbb898992eaf7c18
- <->
2024-02-20 11:49:10 +0300

    missing-number: heapq, a bit slower than last

--- leetcode/missing-number.py
+++ leetcode/missing-number.py
@@ -1,13 +1,25 @@
+import heapq
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
+        heapq.heapify(nums)
         index = 0
-        for i in range(len(nums) + 1):
-            if index == len(nums):
-                return index
-            if index != nums[i]:
+        while nums:
+            num = heapq.heappop(nums)
+            if num != index:
                 return index
             index += 1
+        return index
+
+# class Solution:
+#     def missingNumber(self, nums: List[int]) -> int:
+#         nums.sort()
+#         index = 0
+#         for i in range(len(nums) + 1):
+#             if index == len(nums):
+#                 return index
+#             if index != nums[i]:
+#                 return index
+#             index += 1
             
 if __name__ == "__main__":
     s = Solution()
