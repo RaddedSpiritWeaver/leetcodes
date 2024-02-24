280a71511d8a7ebf9e010c7753a982a42393ed2e
- <->
2024-02-24 23:49:48 +0300

    perfect-squares: accepted. decent performance

--- leetcode/perfect-squares.py
+++ leetcode/perfect-squares.py
@@ -1,3 +1,5 @@
+from timeit import default_timer as timer
+
 class Solution:
     def numSquares(self, n: int) -> int:
         #   start with 0
@@ -6,7 +8,9 @@ class Solution:
             el = queue.pop(0)
             num = el[0]
             #   since max n is 10^4, expand potential children
-            for i in range(1,100):
+            rng = list(range(1,101))
+            rng = rng[::-1]
+            for i in rng:
                 res = num + i ** 2
                 #   fast return found
                 if res == n:
@@ -14,9 +18,12 @@ class Solution:
                 if res < n:
                     queue.append([res, el[1] + 1])
                 if res > n:
-                    break
+                    continue
                 
 if __name__ == "__main__":
+    start = timer()
     s = Solution()
     n = 7168
     print(s.numSquares(n))
+    end = timer()
+    print(start - end)
