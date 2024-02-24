2c1fe29f6c7ae8d310f6ab0f8e661a2b7805381d
- <->
2024-02-24 21:21:09 +0300

    people-with-secret: accepted, much faster response time

--- leetcode/people-secrets.py
+++ leetcode/people-secrets.py
@@ -3,40 +3,58 @@ from typing import List, Dict, Set
 # TODO: we can do only one for on the sorted meetings based on time, and hold meetings form a certain time stamp
 # and if we get a known person, we will update all the meetings in that time frame
 
+# class Node:
+#     def __init__(self,label:int, rank:int):
+#         self.label = label
+#         self.rank = rank
+#         self.children = []
+        
+#     def add_child()
+
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        #   sort meetings based on their time
-        # meetings.append([0, first_person, 0])
-
-        #   hold all the people that meet at time i in set, and i as the key
-        chronology: Dict[int ,List]= {}
-        for p1, p2, t in meetings:
-            try:
-                chronology[t].append([p1, p2])
-            except KeyError:
-                chronology[t] = [[p1, p2]]
-                
-        meetings_at_t = [[k, chronology[k]] for k in chronology.keys()]
-        meetings_at_t.sort(key= lambda x: x[0])
-
-        know_secret = set([0, first_person])
-        for t, meetings_t in meetings_at_t:
-            check = True
-            while check:
-                update = []
-                for i in range(len(meetings_t)):
-                    if (meetings_t[i][0] in know_secret) or (meetings_t[i][1] in know_secret):
-                        know_secret.update([meetings_t[i][0], meetings_t[i][1]])
-                        update.append(i)
-                if not update:
-                    check = False
-                else:
-                    for i in sorted(update, reverse=True):
-                        del meetings_t[i]
-                    
-                    
+        knows = [False for _ in range(n)]
+        knows[0] = True
+        knows[firstPerson] = True
+        #   stores the min time knoews
+        time_knew = [100_001 for _ in range(n)]
+        time_knew[0] = 0
+        time_knew[firstPerson] = 0
         
-        return list(know_secret)
+        rels = [[] for _ in range(n)]
+        
+        meetings.sort(key= lambda x: x[2])
+        for p1, p2, t in meetings:
+            rels[p1].append([p2, t])
+            rels[p2].append([p1, t])
+            
+        def handle(person_index):
+            #   iterate over its relations
+            p_rels = rels[person_index]
+            #   mark relations that have a knowing time after the event
+            marked_indecies = []
+            for i in range(len(p_rels)):
+                #   time of meeting before knowledge
+                if p_rels[i][1] < time_knew[person_index]:
+                    continue
+                
+                knows[p_rels[i][0]] = True
+                
+                if time_knew[p_rels[i][0]] > p_rels[i][1]:
+                    time_knew[p_rels[i][0]] = p_rels[i][1]
+                    marked_indecies.append(i)
+                    #   update the child again
+            for i in sorted(marked_indecies, reverse=True):
+                child = p_rels[i][0]
+                del p_rels[i]
+                handle(child)
+                
+        for i in range(n):
+            if knows[i]:
+                handle(i)
+                
+        return [x for x in range(len(knows)) if knows[x]]
+            
     
 if __name__ == "__main__":
     s = Solution()
