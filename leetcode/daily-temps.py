27528ccf2a21090eb6ad94e285903aca2153b959
- <->
2024-03-01 22:40:30 +0300

    daily-temps: stack method, order n

--- leetcode/daily-temps.py
+++ leetcode/daily-temps.py
@@ -1,21 +1,27 @@
-from traitlets import List
-
+from typing import List
 
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         res = [0 for _ in range(len(temperatures))]
-        for i in range(len(temperatures) - 1):
-            j = 1
-            while temperatures[i] >= temperatures[i + j]:
-                j += 1
-                if i + j >= len(temperatures):
-                    break
+        stack = []
+        for i in range(len(temperatures)):
+            #   push value and index at the same time
+            if not stack:
+                stack.append((temperatures[i], i))
             else:
-                res[i] = j
+                #   when pushing to the stack, pop smaller values
+                poped = []
+                while stack[-1][0] < temperatures[i]:
+                    poped.append(stack.pop())
+                    if not stack: break
+                #   for those poped values, process their result dif  
+                for _ , j in poped:
+                    res[j] = i - j
+                stack.append((temperatures[i], i))
         return res
         
         
 if __name__ == "__main__":
     sol = Solution()
-    temperatures = [73,74,75,71,69,72,76,73]
+    temperatures = [30,40,50,60]
     print(sol.dailyTemperatures(temperatures=temperatures))
