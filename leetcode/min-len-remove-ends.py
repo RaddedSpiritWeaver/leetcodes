838b002ca1a0af903c61fd811de9c132b99048a3
- <->
2024-03-05 12:00:45 +0300

    string-remove-similar-ends: cleaner implementation

--- leetcode/min-len-remove-ends.py
+++ leetcode/min-len-remove-ends.py
@@ -2,21 +2,17 @@ class Solution:
     def minimumLength(self, s: str) -> int:
         begin, end = 0, len(s) - 1
         
-        while begin < end:
-            if s[begin] == s[end]:
-                segment_char = s[begin]
+        while begin < end and s[begin] == s[end]:
+            segment_char = s[begin]
+            begin += 1
+            end -= 1
+            #   try to move begin
+            while begin <= end and segment_char == s[begin]:
                 begin += 1
+            #   try to move end
+            while begin <= end and segment_char == s[end]:
                 end -= 1
-                #   try to move begin
-                while begin <= end and segment_char == s[begin]:
-                    begin += 1
-                #   try to move end
-                while begin <= end and segment_char == s[end]:
-                    end -= 1
-            else:
-                break
-        s = s[begin:end + 1]
-        print(s)
+
         return end - begin + 1
     
 if __name__ == "__main__":
