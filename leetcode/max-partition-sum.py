c985a78b9bcebea3c0953c132011aa2f8060368b
- <->
2024-03-08 00:50:09 +0300

    max-partition-sum: works a bit, but wrong logic

--- leetcode/max-partition-sum.py
+++ leetcode/max-partition-sum.py
@@ -5,23 +5,6 @@ in a dp way, we should first solve it form k = 1 and then build up to the actual
 
 select the min values and merge them with their highest neighbor
 """
-
-class GroupNode:
-    def __init__(self, value, range):
-        self.value = value
-        self.range = range
-        self.right = None
-        self.left = None
-
-# mostleft = left = GroupNode(arr[0], (0, 1))
-#         for i in range(1,len(arr)):
-#             right = GroupNode(arr[i], (i, i+1))
-#             left.right = right
-#             right.left = left
-#             left = right
-#         pass
-
-
 class Solution:
     
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
@@ -36,19 +19,21 @@ class Solution:
             group = groups[target]
             if group != small_group and groups.count(group) < kk:
                 #   merge
-                groups[smallest_val_index] = group
-                arr[smallest_val_index] = left_val
-                heapq.heappush(heap, left_val)
+                groups[small_index] = group
+                arr[small_index] = target_val
+                heapq.heappush(heap, target_val)
                 return True
             return False
         
         for kk in range(2, k + 1):
             go_next = False
+            processed = [False] * len(arr)
             while heap and not go_next:
                 smallest_val = heapq.heappop(heap)
                 indices = [i for i in range(len(arr)) if arr[i] == smallest_val]
                 for smallest_val_index in indices:
                     #   check its left and right values
+                    processed[smallest_val_index] = True
                     left = smallest_val_index - 1
                     right = smallest_val_index + 1
                     left_val = arr[left] if left >= 0 else -1
@@ -57,39 +42,20 @@ class Solution:
                     #   prioritize left
                     if left_val >= right_val:
                         #   left
-                        #   check the group
-                        group = groups[left]
-                        if group != cur_group and groups.count(group) < kk:
-                            #   merge
-                            groups[smallest_val_index] = group
-                            arr[smallest_val_index] = left_val
-                            heapq.heappush(heap, left_val)
-                            continue
-                        group = groups[right]
-                        if group != cur_group and groups.count(group) < kk:
-                            #   merge
-                            groups[smallest_val_index] = group
-                            arr[smallest_val_index] = right_val
-                            heapq.heappush(heap, right_val)
-                            continue
+                        if check_and_merge(left, left_val, smallest_val_index, cur_group): break
+                        elif check_and_merge(right, right_val, smallest_val_index, cur_group): break
+                        else: 
+                            heapq.heappush(heap, smallest_val)
+                            go_next = True
                     else:
-                        group = groups[right]
-                        if group != cur_group and groups.count(group) < kk:
-                            #   merge
-                            groups[smallest_val_index] = group
-                            arr[smallest_val_index] = right_val
-                            heapq.heappush(heap, right_val)
-                            continue
-                        group = groups[left]
-                        if group != cur_group and groups.count(group) < kk:
-                            #   merge
-                            groups[smallest_val_index] = group
-                            arr[smallest_val_index] = left_val
-                            heapq.heappush(heap, left_val)
-                            continue
-                
-                heapq.heappush(heap, smallest_val)
-                go_next = True
+                        #   right
+                        if check_and_merge(right, right_val, smallest_val_index, cur_group): break
+                        elif check_and_merge(left, left_val, smallest_val_index, cur_group): break
+                        else: 
+                            heapq.heappush(heap, smallest_val)
+                            go_next = True
+                # else:
+                #     go_next = True
                 
                 
                 
@@ -100,6 +66,8 @@ class Solution:
     
 if __name__ == "__main__":
     sol = Solution()
+    # arr = [1,4,1,5,7,3,6,1,9,9,3]
+    # k = 4
     arr = [1,15,7,9,2,5,10]
     k = 3
     print(sol.maxSumAfterPartitioning(arr, k))
