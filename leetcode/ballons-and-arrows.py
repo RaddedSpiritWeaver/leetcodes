e5c9e67cce00bd6bcf3af212b4b7ed3ac92ed780
- <->
2024-03-18 16:42:12 +0300

    ballons-and-arrows: init, wrong answer

---
+++ leetcode/ballons-and-arrows.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        areas = []
+        for point in points:
+            for region in areas:
+                #   if intersect, then merge
+                if point[1] < region[0] or region[1] < point[0]:
+                    continue
+                else:
+                    region[0] = max(point[0], region[0])
+                    region[1] = min(region[1], point[1])
+                    break
+            else:
+                areas.append(point)
+        
+        return len(areas)
+    
+if __name__ == "__main__":
+    sol = Solution()
+    points = [[3,9],[7,12],[3,8],[6,8],[9,10],[2,9],[0,9],[3,9],[0,6],[2,8]]
+    print(sol.findMinArrowShots(points=points))
+    
